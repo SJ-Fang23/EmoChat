681342a8564fe0d0fa3574276c3b7f2d1a13f39e
Turner Hayes <dev@turnerhayes.com>
2023-11-14 20:41:28 -0500

    Add get_sentiment script

---
+++ get_sentiment.py
@@ -0,0 +1,35 @@
+import spacy
+from typing import Optional
+from enum import Enum
+import spacytextblob.spacytextblob
+
+
+nlp: Optional[spacy.language.Language] = None
+def get_nlp() -> spacy.language.Language:
+    global nlp
+    if nlp is None:
+        nlp = spacy.load('en_core_web_sm')
+        nlp.add_pipe('spacytextblob')
+    return nlp
+
+
+Sentiment = Enum("Polarity", ["POSITIVE", "NEUTRAL", "NEGATIVE"])
+
+def get_sentiment(
+        text: str,
+        negative_threshold: float = -0.2,
+        positive_threshold: float = 0.2,
+        debug: bool = False
+) -> Sentiment:
+    nlp = get_nlp()
+
+    doc = nlp(text)
+    polarity = doc._.blob.polarity
+    if debug:
+        print("Polarity: {0}".format(polarity))
+    if polarity < negative_threshold:
+        return Sentiment.NEGATIVE
+    elif polarity > positive_threshold:
+        return Sentiment.POSITIVE
+    else:
+        return Sentiment.NEUTRAL
---
+++ test_get_sentiment.py
@@ -0,0 +1,9 @@
+from get_sentiment import get_sentiment
+import sys
+import os
+
+text = sys.argv[1] or "It was the best of times, it was the worst of times."
+
+sentiment = get_sentiment(text, debug=not not os.environ.get("DEBUG"))
+
+print("Sentiment: {0}".format(sentiment))
